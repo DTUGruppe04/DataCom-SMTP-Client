bba1c66f088db3aa4bb2a96015a5fa141d652c8f
valde12 <s224289@DTU.dk>
2023-03-07 13:36:57 +0100

    lavet færdig basic opgaven

--- SMTP Client/src/SMTPConnection.java
+++ SMTP Client/src/SMTPConnection.java
@@ -14,7 +14,7 @@ public class SMTPConnection {
     private BufferedReader fromServer;
     private DataOutputStream toServer;
 
-    private static final int SMTP_PORT = 25;
+    private static final int SMTP_PORT = 2552;
     private static final String CRLF = "\r\n";
 
     /* Are we connected? Used in close() to determine what to do. */
@@ -23,9 +23,13 @@ public class SMTPConnection {
     /* Create an SMTPConnection object. Create the socket and the 
        associated streams. Initialize SMTP connection. */
     public SMTPConnection(Envelope envelope) throws IOException {
-        // connection = /* Fill in */;
-        fromServer = /* Fill in */;
-        toServer =   /* Fill in */;
+        connection = new Socket("datacomm.bhsi.xyz",SMTP_PORT);
+        fromServer = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        toServer =  new DataOutputStream(connection.getOutputStream());
+
+        if (parseReply(fromServer.readLine()) != 220) {
+            throw new IOException();
+        }
 
         /* Fill in */
 	/* Read a line from server and check that the reply code is 220.
@@ -34,8 +38,8 @@ public class SMTPConnection {
 
 	/* SMTP handshake. We need the name of the local machine.
 	   Send the appropriate SMTP handshake command. */
-        String localhost = /* Fill in */;
-        sendCommand( /* Fill in */ );
+        String localhost = InetAddress.getLocalHost().getHostName();
+        sendCommand("helo " + localhost, 250);
 
         isConnected = true;
     }
@@ -44,6 +48,11 @@ public class SMTPConnection {
        correct order. No checking for errors, just throw them to the
        caller. */
     public void send(Envelope envelope) throws IOException {
+        sendCommand("mail from: <" + envelope.Sender + ">", 250);
+        sendCommand("rcpt to: <" + envelope.Recipient + ">", 250);
+        sendCommand("data", 354);
+        toServer.writeBytes(envelope.Message + CRLF);
+        sendCommand(".", 250);
         /* Fill in */
 	/* Send all the necessary commands to send a message. Call
 	   sendCommand() to do the dirty work. Do _not_ catch the
@@ -56,8 +65,8 @@ public class SMTPConnection {
     public void close() {
         isConnected = false;
         try {
-            sendCommand( /* Fill in */ );
-            // connection.close();
+            sendCommand("quit", 221);
+            connection.close();
         } catch (IOException e) {
             System.out.println("Unable to close connection: " + e);
             isConnected = true;
@@ -65,8 +74,14 @@ public class SMTPConnection {
     }
 
     /* Send an SMTP command to the server. Check that the reply code is
-       what is is supposed to be according to RFC 821. */
+       what is supposed to be according to RFC 821. */
     private void sendCommand(String command, int rc) throws IOException {
+        toServer.writeBytes(command + CRLF);
+        int reply = parseReply(fromServer.readLine());
+        if (reply != rc){
+            throw new IOException("Error, reply code is: " + reply + " and should be: " + rc);
+        }
+        System.out.println("send success: " + command + reply);
         /* Fill in */
         /* Write command to server and read reply from server. */
         /* Fill in */
@@ -79,6 +94,7 @@ public class SMTPConnection {
 
     /* Parse the reply line from the server. Returns the reply code. */
     private int parseReply(String reply) {
+        return Integer.parseInt(reply.substring(0,3));
         /* Fill in */
     }
 
